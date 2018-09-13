1e44c47d22fca7c210240c8947a4451b7532c64f
fearlesspandas <landonrenzullo@gmail.com>
2018-09-13 13:36:30 -0400

    update login controller

--- src/main/java/com/revature/controller/LoginController.java
+++ src/main/java/com/revature/controller/LoginController.java
@@ -1,49 +1,45 @@
 package com.revature.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
+import com.revature.beans.Credentials;
+import com.revature.domain.User;
+import com.revature.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
-import com.revature.beans.Credentials;
-import com.revature.domain.User;
-import com.revature.service.UserService;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller("loginController")
 @RequestMapping("/login")
 public class LoginController {
-	@Autowired
+    @Autowired
     private UserService userService;
-	
-	@GetMapping
-	public String getLogin(){
-		return "forward:/static/Login.html";
-	}
-	 @PostMapping
-	 @ResponseBody
-	    public ResponseEntity<String> loginUser(HttpServletRequest req, @RequestBody Credentials credentials){
-		 System.out.println("Logging in");
-	    	try {
-	    		User u = userService.getUserByLogin(credentials.getUsername(),credentials.getPass());
-	    		if(u != null) {
-	    			req.getSession(true).setAttribute("id", u.getUserId());
-	    			return new ResponseEntity<>("Successfully logged in as" + u.getUsername(),HttpStatus.OK);
-	    		}else {
-	    			return new ResponseEntity<>("Username or Password are incorrect",HttpStatus.OK);
-	    		}
-	    		
-	    	}catch 	(Exception e) {
-	    		e.printStackTrace();
-	    	}
-	    	return null;
-	    }
+
+    @GetMapping
+    public String getLogin() {
+        return "forward:/static/views/login.html";
+    }
+
+    @PostMapping
+    public RedirectView loginUser(HttpServletRequest req, @RequestBody Credentials credentials) {
+        System.out.println("Logging in");
+        try {
+            User u = userService.getUserByLogin(credentials.getUsername(), credentials.getPass());
+            if (u != null) {
+                req.getSession(true).setAttribute("id", u.getUserId());
+                return new RedirectView("/profile");
+            } else {
+                return new RedirectView("/login");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }

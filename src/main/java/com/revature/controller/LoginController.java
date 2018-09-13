2ac2de9a759d6188ce697d5acbc820d753aace72
Jeremy Straus <Jstraws@verizon.net>
2018-09-13 13:48:52 -0400

    Working redirect

--- src/main/java/com/revature/controller/LoginController.java
+++ src/main/java/com/revature/controller/LoginController.java
@@ -1,48 +1,43 @@
 package com.revature.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.revature.beans.Credentials;
 import com.revature.domain.User;
 import com.revature.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller("loginController")
 @RequestMapping("/login")
 public class LoginController {
-	@Autowired
+    @Autowired
     private UserService userService;
-	
-	@GetMapping
-	public String getLogin(){
-		return "forward:/static/Login.html";
-	}
-	 @PostMapping
-	    public ResponseEntity<String> loginUser(HttpServletRequest req, @RequestBody Credentials credentials){
-		 System.out.println("Logging in");
-	    	try {
-	    		User u = userService.getUserByLogin(credentials.getUsername(),credentials.getPass());
-	    		if(u != null) {
-	    			req.getSession(true).setAttribute("id", u.getUserId());
-	    			return new ResponseEntity<>("Successfully logged in as" + u.getUsername(),HttpStatus.OK);
-	    		}else {
-	    			return new ResponseEntity<>("Username or Password are incorrect",HttpStatus.OK);
-	    		}
-	    		
-	    	}catch 	(Exception e) {
-	    		e.printStackTrace();
-	    	}
-	    	return null;
-	    }
+
+    @GetMapping
+    public String getLogin() {
+        return "forward:/static/views/login.html";
+    }
+
+    @PostMapping
+    @ResponseBody
+    public String loginUser(HttpServletRequest req, @RequestBody Credentials credentials) {
+        System.out.println("Logging in");
+        try {
+            User u = userService.getUserByLogin(credentials.getUsername(), credentials.getPass());
+            if (u != null) {
+                req.getSession(true).setAttribute("id", u.getUserId());
+                return "/profile";
+            } else {
+                return "/login";
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
--- src/main/webapp/WEB-INF/static/js/profile.js
+++ src/main/webapp/WEB-INF/static/js/profile.js
@@ -12,25 +12,23 @@ function sendAjaxGet(url, func) {
 function populateUser(xhr) {
     if (xhr.responseText) {
         var res = JSON.parse(xhr.responseText);
-        // var table = document.getElementById("employeeTable");
-        // var row = document.createElement("tr");
-        // table.appendChild(row);
-        // var firstName = document.createElement("td");
-        // firstName.innerText = res[i].firstName;
-        // var lastName = document.createElement("td");
-        // lastName.innerText = res[i].lastName;
-        // var userName = document.createElement("td");
-        // userName.innerText = res[i].username;
-        // var email = document.createElement("td");
-        // email.innerText = res[i].email;
-        // row.append(firstName, lastName, userName, email);
         var div = document.getElementById("profileInfo");
         var name = document.createElement("h3");
         name.innerText = res.fName + " " + res.lName;
+        var username = document.createElement("p");
+        username.innerText = "Username: " + res.username;
+        var email = document.createElement("p");
+        email.innerText = "Email: " + res.email;
+        var pemail = document.createElement("p");
+        pemail.innerText = "Pay Email: " + res.payEmail;
+        var address = document.createElement("p");
+        var tempAd = res.addresses[0];
+        address.innerText = "Address: " + tempAd.street + " " + tempAd.apartment + " " + tempAd.city + ", " + tempAd.state + " " + tempAd.zip;
+        div.append(name, username, email, pemail, address);
     }
 }
 
 
 window.onload = function () {
-    sendAjaxGet("/data?entity=employee&get=managed", populateUser);
+    sendAjaxGet("/user/22", populateUser);
 };

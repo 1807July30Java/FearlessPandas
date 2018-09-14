cff93ff72806f04500004eaa0e2506fd67511519
fearlesspandas <landonrenzullo@gmail.com>
2018-09-13 20:41:34 -0400

    new auction page + bug fixes

--- src/main/java/com/revature/controller/BookConditionController.java
+++ src/main/java/com/revature/controller/BookConditionController.java
@@ -1,8 +1,11 @@
 package com.revature.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +23,10 @@ public class BookConditionController {
 	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
 	public ResponseEntity<BookCondition> getBookCondition(@PathVariable int id){
 		return new ResponseEntity<>(bookConditionService.getBookConditionById(id),HttpStatus.OK);
+	}
+	@GetMapping("/all")
+	public ResponseEntity<List<BookCondition>> getAllConditions(){
+		return new ResponseEntity<>(bookConditionService.getAllConditions(),HttpStatus.OK);
 	}
 	
 	
